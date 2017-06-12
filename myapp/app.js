e0939415181313c4794b9eea425976af3ef1eb21
Ayush Goyal <ayush987goyal@gmail.com>
2017-06-13 01:47:00 +0530

    just some tests

--- myapp/app.js
+++ myapp/app.js
@@ -2,19 +2,27 @@ const express = require('express')
 const app = express()
 var Git = require('nodegit')
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send('Hello World!')
 })
 
-app.get('/rep', function (req, res) {
-    Git.Clone("https://ag65856@cedt-icg-bitbucket.nam.nsroot.net/bitbucket/scm/thorcobra/dosa_cobra_config.git", "repo").then(function (repository) {
-        console.log(repository)
-        // Work with the repository object here.
-    }).catch(function (err) {
+app.get('/rep', function(req, res) {
+    let temp = false;
+    Git.Clone("https://github.com/ayush987goyal/express.git", "repo").then(function(repository) {
+        //console.log(repository)
+        temp = true;
+    }).catch(function(err) {
+        temp = false;
         console.log(err);
     });
+    if (temp) {
+        res.send("Done!");
+    } else {
+        res.end();
+    }
+
 })
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
 })
